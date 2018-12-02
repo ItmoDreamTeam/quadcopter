8a428d93ede61402800e9eae56465171e16588da
Kirill Smirnov <smirnowku@gmail.com>
2018-12-02 19:57:24 +0300

    Adjust speeds

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 import integrator
 
 # Coordinates to achieve by the end of the flight
@@ -9,9 +12,25 @@ z_desired = 0
 interval = 0.1
 
 # Engines velocities
-ω1 = [0, 340]
-ω2 = [0, 340]
-ω3 = [0, 340]
-ω4 = [0, 340]
+ω1 = [495] * 10
+ω2 = [507] * 10
+ω3 = [487] * 10
+ω4 = [504] * 10
 
 x, y, z = integrator.integrate(ω1, ω2, ω3, ω4, interval)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, aspect='equal', projection='3d')
+
+ax.plot(x, y, z, "g.-")
+ax.plot([x[0]], [y[0]], [z[0]], ".r")
+
+ax.set_xlim3d(min(x + [-1]), max(x + [1]))
+ax.set_ylim3d(min(y + [-1]), max(y + [1]))
+ax.set_zlim3d(0, max(z + [1]))
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+fig.show()
